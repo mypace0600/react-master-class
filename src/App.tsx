471425fe3ff125027116b1b7cb8aa153879e29ec
mypace0600 <mypace0600@gmail.com>
2024-01-25 16:35:50 +0900

    browserRouter 예시

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import { useState } from "react";
 import { styled } from "styled-components";
-
-const Container = styled.div`
-  background-color:${(props)=>props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color:${(props)=>props.theme.textColor};
-`;
-
-const Btn = styled.button`
-  background-color:${(props)=>props.theme.btnColor};
-`;
+import Router from "./router";
 
 function App() {
   return (
-    <Container>
-      <H1>hello</H1>
-      <Btn>button</Btn>
-    </Container>
+    <Router />
   );
 }
 
